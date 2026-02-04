dfaba8ab67532dabf3a2c9b01b29f2ef78f9a173
Host <host@sspbrno.cz>
2026-02-04 14:29:36 +0100

    save_commit

--- tank-busters/Scripts/player_1.gd
+++ tank-busters/Scripts/player_1.gd
@@ -5,9 +5,12 @@ var spawn_position = Vector2(0,130)
 
 @onready var timer: Timer = $Timer
 
+@onready var marker_2d: Marker2D = $Marker2D
+const BULLET = preload("res://Scenes/bullet.tscn")
+
+
 
 # arrow keys movement
-
 func _physics_process(delta: float) -> void:
 	var direction = Vector2(
 		Input.get_axis("ui_left", "ui_right"),
@@ -23,13 +26,22 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
-func reset_player():
-	queue_free()
-	timer.start()
+	if Input.is_key_pressed(KEY_KP_0):
+		shoot()
+		
 	
+func reset_player():
+	hide()
+	timer.start(3)
+	
+func _on_timer_timeout() -> void:
 	global_position = spawn_position
 	rotation_degrees = 0
-
-
-func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	show()
+	timer.stop()
+	
+	
+func shoot():
+	var new_bullet = BULLET.instantiate()
+	new_bullet.position = marker_2d.global_position
+	get_tree().current_scene.add_child(new_bullet)
